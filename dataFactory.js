1734b0d22fc2ebb92492f17c240dccbfd7f8985c
ghirlekar <gaurav.hirlekar@gmail.com>
2015-09-25 23:35:05 -0400

    Fixed several bugs in dataFactory & now tested confirmed working

--- dataFactory.js
+++ dataFactory.js
@@ -1,4 +1,4 @@
-var mongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 var mongoURL = 'mongodb://localhost:27017/ZigBeeBaseStation',
     mongoCollection = 'temperatures';
 
@@ -15,24 +15,26 @@ MongoClient.connect(mongoURL, function(err, db) {
 function dataFactory(startDate, endDate) {
     data = [];
     deviceList = [];
-    for (var i = 0, i < 4, i++)
+    for (var i = 0; i < 4; i++)
         deviceList.push('0013a200' + String(Math.floor(Math.random() * 90000000) + 10000000));
 
-    for (var i = startDate, i < endDate, i += 300000) {
-        data.push({
-            time: i,
-            temperatures: []
-        });
+    for (var i = startDate; i < endDate; i += 300000) {
+        var temps = [];
         deviceList.forEach(function(device, index) {
             var date = new Date(i);
-            var val = 15 - 5 * Math.cos(Math.PI / 24 * (date.getHours() + date.getMinutes() / 60)) - 20 * Math.cos(Math.PI / 12 * (date.getMonth() + date.getDate() / 31)) + (Math.random() - 0.5);
+            var val = 15 - 5 * Math.cos(2 * Math.PI / 24 * (date.getHours() + date.getMinutes() / 60)) - 20 * Math.cos(2 * Math.PI / 12 * (date.getMonth() + date.getDate() / 31)) + (Math.random() - 0.5);
             if (index === 1 && i < (startDate + (endDate - startDate) / 3)) val = null;
             else if (index === 2 && i > (endDate - (endDate - startDate) / 3)) val = null;
             else if (Math.random() < 0.1) val = null;
-            data.temperatures.push({
+            debugger;
+            if (val) temps.push({
                 deviceID: device,
-                value: val.toString(2)
+                value: val.toFixed(2)
             });
+        });
+        data.push({
+            time: i,
+            temperatures: temps
         });
     };
     return data;
